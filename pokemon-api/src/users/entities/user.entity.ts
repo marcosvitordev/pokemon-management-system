ff00a52ff143cf690881e91d0bb518853fc66457
marcosvitordev <marcosvitorlima531@gmail.com>
2026-03-07 13:20:09 -0300

    feat: create user entity

---
+++ pokemon-api/src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 150 })
+  name: string;
+
+  @Column({ unique: true, length: 150 })
+  email: string;
+
+  @Column({ name: 'password_hash' })
+  passwordHash: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
