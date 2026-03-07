f6e32517e6474e712af48aea8e5eb039d2c3c475
marcosvitordev <marcosvitorlima531@gmail.com>
2026-03-07 13:18:04 -0300

    feat: configure TypeORM with PostgreSQL

--- pokemon-api/src/app.module.ts
+++ pokemon-api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -11,6 +12,21 @@ import { PokemonsModule } from './pokemons/pokemons.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: Number(configService.get<string>('DATABASE_PORT')),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
     AuthModule,
     UsersModule,
     PokemonsModule,
