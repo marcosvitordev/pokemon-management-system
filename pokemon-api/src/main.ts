ddd55bf796285533411660b8a1654ef7ddca533e
marcosvitordev <marcosvitorlima531@gmail.com>
2026-03-08 13:48:29 -0300

    chore: inicializa projeto frontend em next.js com typescript e tailwind

--- pokemon-api/src/main.ts
+++ pokemon-api/src/main.ts
@@ -1,3 +1,43 @@
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Pokemon API')
+//     .setDescription('API REST para gerenciamento de Pokemons com autenticação JWT')
+//     .setVersion('1.0')
+//     .addBearerAuth(
+//       {
+//         type: 'http',
+//         scheme: 'bearer',
+//         bearerFormat: 'JWT',
+//         in: 'header',
+//         name: 'Authorization',
+//         description: 'Informe o token JWT',
+//       },
+//       'Bearer',
+//     )
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -5,6 +45,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:8080', 'http://localhost:3000', 'http://127.0.0.1:3001'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
